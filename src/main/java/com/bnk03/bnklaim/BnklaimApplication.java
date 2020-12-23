0110bce495b8ea43c1af2ec88d6461dbb78055cc
Natthapong <natthapong41@gmail.com>
2020-12-23 16:16:33 +0700

    Init

---
+++ src/main/java/com/bnk03/bnklaim/BnklaimApplication.java
@@ -0,0 +1,13 @@
+package com.bnk03.bnklaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnklaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnklaimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnk03/bnklaim/BnklaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnk03.bnklaim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnklaimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
