6bb421653fc59976a7e8e5b498264ea7e7b7e9f0
Natthapong Jiemjintanarom <natthapong41@gmail.com>
2020-12-27 12:51:12 +0700

    Recreated project and finish register module

--- src/main/java/com/bnk03/bnklaim/BnklaimApplication.java
+++ src/main/java/com/bnk03/bnklaim/BnklaimApplication.java
@@ -1,18 +1,16 @@
 package com.bnk03.bnklaim;
 
-import org.springframework.boot.CommandLineRunner;
+import com.bnk03.bnklaim.service.AccountService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BnklaimApplication implements CommandLineRunner {
+public class BnklaimApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BnklaimApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("\n\n\n\n==========RUN==========\n\n\n\n");
-	}
 }
