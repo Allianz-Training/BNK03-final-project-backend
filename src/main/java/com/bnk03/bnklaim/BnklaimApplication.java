3f92a7119aaa39df74a518eb89d0c7faa67075f7
nadiandnd <ndtaeminnie@gmail.com>
2020-12-30 10:36:09 +0700

    something

--- src/main/java/com/bnk03/bnklaim/BnklaimApplication.java
+++ src/main/java/com/bnk03/bnklaim/BnklaimApplication.java
@@ -1,6 +1,6 @@
 package com.bnk03.bnklaim;
 
-import com.bnk03.bnklaim.entity.UserInformation;
+//import com.bnk03.bnklaim.entity.UserInformation;
 import com.bnk03.bnklaim.repositories.UserInformationRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +10,10 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BnklaimApplication {
-	@Autowired
-	private UserInformationRepository userInformationRepository;
-
-	private UserInformation uInfo = new UserInformation();
+//	@Autowired
+//	private UserInformationRepository userInformationRepository;
+//
+//	private UserInformation uInfo = new UserInformation();
 
 	public static void main(String[] args) {
 		SpringApplication.run(BnklaimApplication.class, args);
--- src/main/java/com/bnk03/bnklaim/controller/ClaimController.java
+++ src/main/java/com/bnk03/bnklaim/controller/ClaimController.java
@@ -2,9 +2,11 @@ package com.bnk03.bnklaim.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @ControllerAdvice
 @RestController
 @RequestMapping("/claim")
